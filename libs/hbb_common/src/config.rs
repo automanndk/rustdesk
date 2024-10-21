f3c0734adf6c166d942eb89ca6e8165df903e509
automanndk <104613127+automanndk@users.noreply.github.com>
2024-10-21 11:14:57 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["39.171.58.253"];
-pub const PUBLIC_RS_PUB_KEY: &str = "YyuKmWR3LgQ6ejKzJ4QrFJRWxBBZVWo6CF6F5GHrpPM=";
+pub const PUBLIC_RS_PUB_KEY: &str = "hztsg123456789!@#$";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
